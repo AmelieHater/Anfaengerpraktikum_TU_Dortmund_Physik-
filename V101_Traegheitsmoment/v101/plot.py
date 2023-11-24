65f851588b6341a8618d6a717555bea5f0468e84
Amelie Hater <amelie.hater@web.de>
2023-11-24 11:04:04 +0100

    Messungen rad angepasst

--- V101_Traegheitsmoment/v101/plot.py
+++ V101_Traegheitsmoment/v101/plot.py
@@ -8,12 +8,12 @@ Federwaagen_Abstand = 0.19975 #in m
 phi_D, F_D = np.genfromtxt("D_Brchnng.txt", unpack=True)
 #print(F_D * Federwaagen_Abstand)
 D_0 = (F_D * Federwaagen_Abstand)/phi_D #in Nm
-print(D_0)
+#print(D_0)
 D_mean = np.mean(D_0)
 #print(D_mean)
 D_std = np.std(D_0)
 D = ufloat(D_mean, D_std)
-print(D)
+print("D ist: ",D)
 
 #Eigentraegheitsmoment der Drillachse 
 Abstand_a, T5_Periodendauer= np.genfromtxt("I_D_Brchnng.txt", unpack=True)
@@ -40,6 +40,28 @@ ax.set_ylabel(r"Periodendauer $\,\,T^2\,\,[s^2]$")
 ax.legend(loc="best")
 fig.savefig("plot.pdf")
 
+#Lineare regression: y = ax+b a = params[0] b = params[1]
+#Durchmesser der Gewichte berechnen mit Fehler
+Blaues_Hoehe0, Blaues_Durchmesser0, Rotes_Hoehe0, Rotes_Durchmesser0 = np.genfromtxt("Gewichte_Maße.txt", unpack=True)
+Gewichte_Durchmesser0 = (Blaues_Durchmesser0 + Rotes_Durchmesser0)/2
+Gewichte_Durchmesser_mean = np.mean(Gewichte_Durchmesser0)
+
+Gewichte_Durchmesser_std = np.std(Gewichte_Durchmesser0)
+Gewichte_Durchmesser = ufloat(Gewichte_Durchmesser_mean, Gewichte_Durchmesser_std)
+print("Durchmesser der Gewichte ist ",Gewichte_Durchmesser)
+#Höhe der Gewichte ausrechnen mit Fehler 
+Gewicht_Hoehe0 = (Blaues_Hoehe0 + Rotes_Hoehe0) /2
+Gewicht_Hoehe_mean = np.mean(Gewicht_Hoehe0)
+Gewicht_Hoehe_std = np.std(Gewicht_Hoehe0)
+Gewicht_Hoehe = ufloat(Gewicht_Hoehe_mean, Gewicht_Hoehe_std)
+print("Hoehe der Gewichte ist: ",Gewicht_Hoehe)
+print("b ist: ", params[1])
+
+I_D = (params[1]*D) / (4 * np.pi**2) - 2 * 0.26115 * np.square(Gewichte_Durchmesser/2) - (1/6) * 0.26115 * np.square(Gewicht_Hoehe)
+Test1 = (params[1]*D) / (4 * np.pi**2)
+Test2 = 2 * 0.26115 * np.square(Gewichte_Durchmesser/2) + (1/6) * 0.26115 * np.square(Gewicht_Hoehe)
+print("I_D ist gleich: ", I_D)
+print("Vorderer Summant ist: ", Test1, "Hinterer Summant ist: ", Test2)
 #print(params[0],params[1])
 
 
