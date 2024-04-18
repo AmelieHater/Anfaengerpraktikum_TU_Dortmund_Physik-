37e30103527aa3ef32728bef3b842d1e7d517ae0
Amelie Hater <amelie.hater@tu-dortmund.de>
2024-04-18 09:10:13 +0200

    Auswertungsversuch der 15. gefühlt

--- V606/v606/plot.py
+++ V606/v606/plot.py
@@ -2,41 +2,45 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-x, y = np.genfromtxt("content/Messwerte/Bandpass_weniger.txt", unpack=True)
+x, y = np.genfromtxt("content/Messwerte/Bandpass.txt", unpack=True)
 
 
-# def gauss(x, a, v):
-#    return np.exp(-a * (x-v)**2)
-def gaussian(x, amplitude, mean, stddev):
-    return amplitude * np.exp(-(((x - mean) / 4 / stddev) ** 2))
+#def gaussian(x, amplitude, mean, stddev):
+#   return amplitude * np.exp(-(((x - mean) / 4 / stddev) ** 2))
+y_norm = y / 104.7618
+#def gaussian(x, a, b):
+#   return np.exp(-(x-b)**2*a)
+#
+x_2 = np.linspace(20, 40, 100)
+#params, covariance_matrix = curve_fit(gaussian, x, y_norm, p0=(31, 13.5))
 
-#def gaussian(x, mean, stddev):
- #   return (1/stddev /np.sqrt(2 * np.pi)) * np.exp(-(((x - mean) / 4 / stddev) ** 2))
 
-
-x_2 = np.linspace(26, 35, 100)
-params, covariance_matrix = curve_fit(gaussian, x, y, p0=(130, 30,0.6))
+def Gauss(x, b, a):
+    return np.exp(-(x-b)**2*a)
+   
+params, pcov = curve_fit(Gauss, x, y_norm, p0 = [31.62, 1])            
+err = np.sqrt(np.diag(pcov))
 
 fig, (ax1) = plt.subplots(1, 1, layout="constrained")
-ax1.plot(x, y, "x", label="Kurve")
-ax1.plot(x_2, gaussian(x_2, *params))
+ax1.plot(x, y_norm, "x", label="Kurve")
+ax1.plot(x_2, Gauss(x_2, *params))
+ax1.hlines(y =(1/np.sqrt(2)), xmin = 31.42, xmax = 31.85, linewidth = 2, linestyle = "-" , color = "green", label = "Güte")
 ax1.set_xlabel("f in kHz")
-ax1.set_ylabel("U in Volt")
+ax1.set_ylabel("U/U_max")
 ax1.legend(loc="best")
 
 fig.savefig("plot.pdf")
-print(params)
+#print(params)
+v_plus = 31.85
+v_minus = 31.42
+v_0 = 31.61188736
+guete = v_0 /(v_plus - v_minus)
+print(guete)
+# Güte ist 73,51601711627912
+# Güte am Gerät war 20 
+Guete_Abweichung = abs(20 - 73.51601711627912)/20
+print(Guete_Abweichung) = 2.675800855813956
+#Abweichung der Güte: 2.675800855813956
 
-# uncertainties = np.sqrt(np.diag(covariance_matrix))
-
-# for name, value, uncertainty in zip("abc", params, uncertainties):
-#    print(f"{name} = {value:8.3f} ± {uncertainty:.3f}")
-
-# print(params)
-
-# from scipy import optimize
-
-# def gaussian(x, amplitude, mean, stddev):
-#    return amplitude * np.exp(-((x - mean) / 4 / stddev)**2)
-
-# popt, _ = optimize.curve_fit(gaussian, x, y, p0=(0.5, 20))
+#Suszeptibilität berechnen 
+# xi = 4 *(F / Q) * (U_Br / U_Sp) mit F: Querschnitt der Spule, 
