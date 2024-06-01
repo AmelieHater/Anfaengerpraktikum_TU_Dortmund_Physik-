bde4f240faa2df6e267279726553885c6f4b6af8
ngocpoc <ngoc-le@hotmail.de>
2024-06-01 13:35:13 +0200

    erstmal fertig

--- V401_Michelson_Interferometer/v401/plot.py
+++ V401_Michelson_Interferometer/v401/plot.py
@@ -8,8 +8,10 @@ def rel_Abweichung(exp, theo):
 
 lambda_theo = 645 * 10**(-9) # m (Diodenlaser)
 u = 5.046
-T_0 = 21.6 + 273.15 # K 
-p_0 = 1019.2 * 10**(-3) # bar
+T_0 = 273.15
+T = 21.6 + 273.15 # K 
+# p_0 = 1019.2 * 100 # Pa
+p_0 = 1013.25 * 100 #Pa
 
 z_0 = np.genfromtxt("content/Messdaten/Messwerte.txt", unpack=True)
 z = ufloat(np.mean(z_0), np.std(z_0)) 
@@ -24,37 +26,32 @@ print(f'Wellenlänge exp = {lambda_exp * 10**9} nm')
 print(f'Wellenlänge theo = {lambda_theo * 10**9} nm')
 
 z1_0, z2_0 = np.genfromtxt("content/Messdaten/Vakuum.txt", unpack=True)
+z1 = ufloat(np.mean(z1_0), np.std(z1_0))
 z2 = ufloat(np.mean(z2_0), np.std(z2_0))
-print(f'z = {z2}')
+print(f'z1 = {z1}')
+print(f'z2 = {z2}')
+
 D = 50 * 10**(-3) # m
-delta_n = (z2 * lambda_theo)/(2*D)
-print(f'delta_n = {delta_n}')
-T = T_0
-T_0 = 273.15
-p = 20 * 10**(-2) # bar
-n_exp = 1 + delta_n * (T/T_0) * (p_0/p)
+delta_n1 = (z1 * lambda_theo)/(2*D)
+delta_n2 = (z2 * lambda_theo)/(2*D)
+print(f'delta_n1 = {delta_n1}')
+print(f'delta_n2 = {delta_n2}')
+
+p = 700 * 133.3 #Pa
+n_exp1 = 1 + delta_n1 * (T/T_0) * (p_0/p)
+n_exp2 = 1 + delta_n2 * (T/T_0) * (p_0/p)
 n_theo = 1.000292
-print(f'n_exp = {n_exp}')
+print(f'Delta p = {p/100} hPa')
+print(f'n_exp1 = {n_exp1}')
+print(f'n_exp2 = {n_exp2}')
+
 
 print('Diskussion')
 lambda_rel = rel_Abweichung(lambda_exp, lambda_theo)
-n_rel = rel_Abweichung(n_exp, n_theo)
+n_rel1 = rel_Abweichung(n_exp1, n_theo)
+n_rel2 = rel_Abweichung(n_exp2, n_theo)
 
 print(f'rel. Abw. Wellenlänge = {lambda_rel} %')
-print(f'rel. Abw. Brechungsindex = {n_rel}')
+print(f'rel. Abw. Brechungsindex 1 = {n_rel1}')
+print(f'rel. Abw. Brechungsindex 2 = {n_rel2}')
 # Umgebungsdruck 1019.2HPa
-# x = np.linspace(0, 10, 1000)
-# y = x ** np.sin(x)
-
-# fig, (ax1, ax2) = plt.subplots(1, 2, layout="constrained")
-# ax1.plot(x, y, label="Kurve")
-# ax1.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-# ax1.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-# ax1.legend(loc="best")
-
-# ax2.plot(x, y, label="Kurve")
-# ax2.set_xlabel(r"$\alpha \mathbin{/} \unit{\ohm}$")
-# ax2.set_ylabel(r"$y \mathbin{/} \unit{\micro\joule}$")
-# ax2.legend(loc="best")
-
-# fig.savefig("build/plot.pdf")
